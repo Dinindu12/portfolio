d5eea610e0531349401092c6b482330f0f8479b3
Dinindu12 <jayampathidinindu@gmail.com>
2026-08-13 09:07:27 +0530

    Add Weather Forecast App to project list

--- script.js
+++ script.js
@@ -54,6 +54,15 @@ const projects = [
     icon: 'fa-qrcode',
     category: 'web'
   }
+  {
+  id: 7,
+  title: 'Weather Forecast App',
+  desc: 'Real-time weather updates with 3-day forecast. Search any city and get temperature, humidity, wind speed, and more.',
+  tech: ['HTML', 'CSS', 'JS', 'API'],
+  link: 'https://dinindu12.github.io/weather-app/',
+  icon: 'fa-cloud-sun',
+  category: 'web'
+}
 ];
 
 // ===== TESTIMONIALS DATA =====
